f58d76f6cc715e56a7c82c51bfe9d7a5ddaabbb6
Nicolas Champdavoine <nich9513@barany.it.uu.se>
2017-02-08 10:51:55 +0100

    fixing bugs, the code works but not graphics part

--- galsim.c
+++ galsim.c
@@ -28,16 +28,17 @@ int main (int argc, char *argv[]){
     int nsteps = atoi(argv[3]);
     int delta_t = atof(argv[4]);
     int graphics = atoi(argv[5]);
-    double d;
-
-    printf("file open \n");
-
+    particule particules[N];
+ 
+    int i,j,p;
+    int A,sum,F;
+    double distancex, distancey;
     double buf[5*N];
+
     if (read_doubles_from_file(5*N, buf, fileName) != 0){
         printf("Error reading file \n");
         return -1;
     }
-    int i;
     for(i = 0; i<N; i++){
     	particules[i].pos_x = buf[i*5 + 0];
     	particules[i].pos_y = buf[i*5 + 1];     
@@ -46,31 +47,28 @@ int main (int argc, char *argv[]){
     	particules[i].vel_y = buf[i*5 + 4];     
     }
     for (i=0; i<nsteps; i++) {
-    for (p=0;p<N;p++) {
-      sum=0;
-      for (j=0;j<N-1;j++) {
-        if (j != p) {
-          distancex = particules[p].pos_x - particules[j].pos_x;
-          distancey = particules[p].pos_y - particules[j].pos_y;
-          sum += particules[j].mass / pow(  pow(distancex,2) - pow(distancey, 2) ,2);
-        }
-      }
-      F = (-100/N) * particules[p].mass * sum;
-      A = F/particules[p].mass;
+        for (p=0;p<N;p++) {
+            sum=0;
+            for (j=0;j<N-1;j++) {
+                if (j != p) {
+                distancex = particules[p].pos_x - particules[j].pos_x;
+                distancey = particules[p].pos_y - particules[j].pos_y;
+                sum += particules[j].mass / pow(  pow(distancex,2) - pow(distancey, 2) ,2); //utilise la 2e formule pour la stabilite
+                }
+            }
+            F = (-100/N) * particules[p].mass * sum; 
+            A = F/particules[p].mass;
 
-      particules[p].pos_x += delta_t*particules[p].vel_x + pow(delta_t,2)*A;
-      particules[p].pos_y += delta_t*particules[p].vel_y + pow(delta_t,2)*A;
-      particules[p].vel_x += delta_t*A;
-      particules[p].vel_y += delta_t*A;
-    }
+            particules[p].pos_x += delta_t*particules[p].vel_x + pow(delta_t,2)*A; // es ce le bon ordre ?
+            particules[p].pos_y += delta_t*particules[p].vel_y + pow(delta_t,2)*A;
+            particules[p].vel_x += delta_t*A;
+            particules[p].vel_y += delta_t*A;
+        }
   }
   for (i=0;i<N;i++) {
     printf("---------------------------------\n");
     printf("x:%f\ny:%f\nmass:%f\nvel_x:%f\nvel_y:%f\n", particules[i].pos_x, particules[i].pos_y, particules[i].mass, particules[i].vel_x, particules[i].vel_y);
   }
-  return 0;
-}
-
 
     if (graphics == 1){
         float L=1, W=1;
@@ -84,7 +82,7 @@ int main (int argc, char *argv[]){
             for (j=0; j<N; j++){
                 /*set each particule*/
                 x = 0;
-                j = 1;
+                y = 1;
                 DrawCircle(x, y, L, W, circleRadius, circleColor);
             }
             Refresh();
