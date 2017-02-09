8cb3a179796543e1678ffd8242c5456609673e5c
Nicolas Champdavoine <nich9513@siegbahn.it.uu.se>
2017-02-09 19:00:18 +0100

    time improvement, by removing all pow

--- galsim.c
+++ galsim.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <sys/time.h>
 
 #include "graphics/graphics.h"
 #include "file_operations/file_operations.h"
 
-const float circleRadius = 0.025, circleColor = 0;
-const int windowWidth = 800;
-
+static double get_wall_seconds() {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  double seconds = tv.tv_sec + (double)tv.tv_usec / 1000000;
+  return seconds;
+}
 
 typedef struct particule {
     double pos_x;
@@ -34,14 +38,16 @@ int main (int argc, char *argv[]){
     particule particules[N];
  
     int i, j, p;
-    double A, F;
-    double rij, cst_j, mj, sum_dist_square, cord_x, cord_y, sum;
+    double rij, cst_j, cord_x, cord_y;
     double distancex, distancey;
     double buf[5*N];
     double output[5*N];
+    double sum_Fx, sum_Fy, Ax, Ay;
 
+    double time1;
     double positions_x[nsteps][N], positions_y[nsteps][N];
 
+    time1 = get_wall_seconds();
     if (read_doubles_from_file(5*N, buf, fileName) != 0){
         printf("Error reading file \n");
         return -1;
@@ -54,42 +60,29 @@ int main (int argc, char *argv[]){
     	particules[i]->vel_x = buf[i*5 + 3];     
     	particules[i]->vel_y = buf[i*5 + 4];     
     }
-    printf("INIT\n");
-    for (i=0; i<N; i++) {
-        printf("---------------------------------\n");
-        printf("x:%lf\ny:%lf\nmass:%lf\nvel_x:%lf\nvel_y:%lf\n", particules[i]->pos_x, particules[i]->pos_y, particules[i]->mass, particules[i]->vel_x, particules[i]->vel_y);
-  }
+  printf("reading files took %7.3f wall seconds.\n", get_wall_seconds()-time1);
     // Here we define sigma(mj/(rpj+Epsilon0)^3),j=0..N-1,j!=p)
+  time1 = get_wall_seconds();
+    double G = -100/N;
     for (p=0; p<nsteps; p++) {
         for (i=0; i<N; i++) {
-            sum_dist_square = 0;
-            double sum_Fx = 0;
-            double sum_Fy = 0;
-           // sum = 0;
+            sum_Fx = 0;
+            sum_Fy = 0;
             for (j=0; j<N; j++) {
-                if (j != i) {
                 distancex = particules[i]->pos_x - particules[j]->pos_x;
                 distancey = particules[i]->pos_y - particules[j]->pos_y;
-                rij = pow(pow(distancex,2) + pow(distancey, 2),0.5);
-                printf("rij %lf\n", rij);
-                mj = particules[j]->mass;
-                cst_j = mj/(pow(rij+E0,3));
+                rij = sqrt(distancex*distancex + distancey*distancey);
+                cst_j = (particules[j]->mass) / ((rij+E0)*(rij+E0)*(rij+E0));
                 cord_x = cst_j * (distancex);
                 cord_y = cst_j * (distancey);
-             //   sum_dist_square += pow(cord_x,2) + pow(cord_y,2);              
-             //   sum += particules[j]->mass /pow(pow(distancex,2)-pow(distancey,2),2); 
                 sum_Fx += cord_x;
                 sum_Fy += cord_y;
-                }
             }
 
-          //  F = (-100/N) * (particules[i]->mass) * pow(sum_dist_square,0.5); 
-          //  A = F/particules[i]->mass;
-            double Ax = (-100/N) * sum_Fx;
-            double Ay = (-100/N) * sum_Fy;
+            Ax = G * sum_Fx;
+            Ay = G * sum_Fy;
             particules[i]->vel_x += delta_t*Ax;
             particules[i]->vel_y += delta_t*Ay;
-            printf("%lf %lf\n", delta_t*Ax, delta_t*Ay);
             particules[i]->pos_x += delta_t*particules[i]->vel_x;
             particules[i]->pos_y += delta_t*particules[i]->vel_y;
 
@@ -97,10 +90,9 @@ int main (int argc, char *argv[]){
             positions_y[p][i] = particules[i]->pos_y;
         }
     }
-    printf("\nFINAL\n");
+  printf("calculations took %7.3f wall seconds.\n", get_wall_seconds()-time1);
+  time1 = get_wall_seconds();
     for (i=0; i<N; i++) {
-        printf("---------------------------------\n");
-        printf("x:%f\ny:%f\nmass:%f\nvel_x:%f\nvel_y:%f\n", particules[i]->pos_x, particules[i]->pos_y, particules[i]->mass, particules[i]->vel_x, particules[i]->vel_y);
         output[i*5 + 0] = particules[i]->pos_x;
         output[i*5 + 1] = particules[i]->pos_y;
         output[i*5 + 2] = particules[i]->mass;
@@ -112,9 +104,14 @@ int main (int argc, char *argv[]){
         printf("Error writing file");
         return -1;
     }
+  printf("writing took %7.3f wall seconds.\n", get_wall_seconds()-time1);
     
-
+    for (i=0; i<N; i++){
+        free(particules[i]);
+    }
     if (graphics == 1){
+        const float circleRadius = 0.05/N, circleColor = 0;
+        const int windowWidth = 800;
         float L=1, W=1;
 	double x, y;
 
@@ -130,7 +127,7 @@ int main (int argc, char *argv[]){
                 DrawCircle(x, y, L, W, circleRadius, circleColor);
             }
             Refresh();
-            usleep(1000);
+            usleep(2000);
         }
         FlushDisplay();
         CloseDisplay();
